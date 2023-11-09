9ddbcc8a6ec3e046578465f3495bca53a60f13f0
hectorLop <lopez.almazan.hector@gmail.com>
2023-11-09 20:03:45 +0100

    Hello world

---
+++ main.zig
@@ -0,0 +1,43 @@
+const std = @import("std");
+
+const Point = struct { x: i16, y: i16 };
+
+pub fn main() void {
+    const image = circle_at_origin();
+
+    for (0..48) |x| {
+        for (0..64) |y| {
+            std.debug.print("{u}", .{image[x][y]});
+        }
+        std.debug.print("\n", .{});
+    }
+}
+
+fn circle_at_origin() [48][64]u8 {
+    var array: [48][64]u8 = undefined;
+    const radius = 5;
+    var curr_point: Point = undefined;
+
+    for (0..48) |x| {
+        for (0..64) |y| {
+            curr_point = Point{ .x = @intCast(x), .y = @intCast(y) };
+
+            if (is_inside_circle(curr_point, radius)) {
+                array[x][y] = '.';
+            } else {
+                array[x][y] = 'x';
+            }
+        }
+    }
+
+    return array;
+}
+
+fn is_inside_circle(point: Point, radius: i16) bool {
+    const center = Point{ .y = 31, .x = 24 };
+    const x: i16 = point.x - center.x;
+    const y: i16 = point.y - center.y;
+
+    const result = std.math.pow(i16, x, 2) + std.math.pow(i16, y, 2);
+    return result <= std.math.pow(i16, radius, 2);
+}
