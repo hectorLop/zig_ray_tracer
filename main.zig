c516677848f2ac2cbc2e6b30253382c923a0123f
hectorLop <lopez.almazan.hector@gmail.com>
2023-11-11 12:04:05 +0100

    Refactor Circle creation

--- main.zig
+++ main.zig
@@ -1,43 +1,63 @@
 const std = @import("std");
 
 const Point = struct { x: i16, y: i16 };
+const Circle = struct { center: Point, radius: i16 };
 
-pub fn main() void {
-    const image = circle_at_origin();
+const Image = struct {
+    width: usize,
+    height: usize,
+    circle: Circle,
+    fn draw_image(self: *Image, allocator: *std.mem.Allocator) ![][]u8 {
+        var matrix = try allocator.alloc([]u8, self.height);
+        for (matrix, 0..) |_, index| {
+            matrix[index] = try allocator.alloc(u8, self.width);
+        }
 
-    for (0..48) |x| {
-        for (0..64) |y| {
-            std.debug.print("{u}", .{image[x][y]});
+        var curr_point: Point = undefined;
+
+        // Optimize cache access by iterating the rows (x) sequentially
+        for (0..self.height) |y| {
+            for (0..self.width) |x| {
+                curr_point = Point{ .x = @intCast(x), .y = @intCast(y) };
+
+                if (is_inside_circle(curr_point, self.circle)) {
+                    matrix[y][x] = '.';
+                } else {
+                    matrix[y][x] = 'x';
+                }
+            }
+        }
+
+        return matrix;
+    }
+};
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    var allocator = gpa.allocator();
+
+    const circle = Circle{ .center = Point{ .x = 32, .y = 24 }, .radius = 5 };
+    var image = Image{ .width = 64, .height = 48, .circle = circle };
+
+    var data: [][]u8 = try image.draw_image(&allocator);
+    defer allocator.free(data);
+
+    print_image(&image, data);
+}
+
+fn print_image(image: *Image, data: [][]u8) void {
+    for (0..image.height) |y| {
+        for (0..image.width) |x| {
+            std.debug.print("{u}", .{data[y][x]});
         }
         std.debug.print("\n", .{});
     }
 }
 
-fn circle_at_origin() [48][64]u8 {
-    var array: [48][64]u8 = undefined;
-    const radius = 5;
-    var curr_point: Point = undefined;
-
-    for (0..48) |y| {
-        for (0..64) |x| {
-            curr_point = Point{ .x = @intCast(x), .y = @intCast(y) };
-
-            if (is_inside_circle(curr_point, radius)) {
-                array[y][x] = '.';
-            } else {
-                array[y][x] = 'x';
-            }
-        }
-    }
-
-    return array;
-}
-
-fn is_inside_circle(point: Point, radius: i16) bool {
-    const center = Point{ .x = 31, .y = 24 };
-    const x: i16 = point.x - center.x;
-    const y: i16 = point.y - center.y;
+fn is_inside_circle(point: Point, circle: Circle) bool {
+    const x: i16 = point.x - circle.center.x;
+    const y: i16 = point.y - circle.center.y;
 
     const result = std.math.pow(i16, x, 2) + std.math.pow(i16, y, 2);
-    return result <= std.math.pow(i16, radius, 2);
+    return result <= std.math.pow(i16, circle.radius, 2);
 }
